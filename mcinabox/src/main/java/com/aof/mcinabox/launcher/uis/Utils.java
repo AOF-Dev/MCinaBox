76ad46d659b38ab8b33412f848034eb7cf856f22
longjunyu2 <877730493@qq.com>
2020-05-31 08:15:37 +0800

    fix a filter

--- mcinabox/src/main/java/com/aof/mcinabox/launcher/uis/Utils.java
+++ mcinabox/src/main/java/com/aof/mcinabox/launcher/uis/Utils.java
@@ -9,7 +9,7 @@ public class Utils {
      * 【匹配字符串在spinner中的位置】
      **/
     public static int getItemPosByString(String str, Spinner spinner) {
-        int count = spinner.getChildCount();
+        int count = spinner.getAdapter().getCount();
         for (int a = 0; a < count; a++) {
             if (spinner.getItemAtPosition(a).equals(str)) {
                 return a;
