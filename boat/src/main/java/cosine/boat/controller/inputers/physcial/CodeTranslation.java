1995b9912d743943ba2f875cb31714664eba0fce
longjunyu2 <877730493@qq.com>
2020-06-12 18:55:31 +0800

    add physcial Keyboard input support to the Base Controller.

---
+++ boat/src/main/java/cosine/boat/controller/inputers/physcial/CodeTranslation.java
@@ -0,0 +1,10 @@
+package cosine.boat.controller.inputers.physcial;
+
+public class CodeTranslation {
+    public static int to(int origin){
+
+        //根据KeyEvent.getCode进行转换
+        //TODO:物理键值 转换为 Lwjgl键值
+        return 0;
+    }
+}
---
+++ boat/src/main/java/cosine/boat/controller/inputers/physcial/KeyboardSupport.java
@@ -0,0 +1,25 @@
+package cosine.boat.controller.inputers.physcial;
+
+import android.view.KeyEvent;
+
+public class KeyboardSupport implements KeyEvent.Callback {
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyMultiple(int keyCode, int count, KeyEvent event) {
+        return false;
+    }
+}
