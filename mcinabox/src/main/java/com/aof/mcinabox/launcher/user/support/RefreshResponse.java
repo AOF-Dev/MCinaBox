2cb7ea6490a0cdbf47e267e7108c25578d47ffe6
longjunyu2 <877730493@qq.com>
2020-10-11 22:17:18 +0800

    [update] Add responses and requests from several specifications for authentication server.

---
+++ mcinabox/src/main/java/com/aof/mcinabox/launcher/user/support/RefreshResponse.java
@@ -0,0 +1,10 @@
+package com.aof.mcinabox.launcher.user.support;
+
+import java.util.UUID;
+
+public class RefreshResponse {
+    public String accessToken;
+    public UUID clientToken;
+    public AuthenticateResponse.Profile selectedProfile;
+
+}
