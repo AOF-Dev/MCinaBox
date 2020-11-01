cc938f131f68bc1b485a4c42e37674aa1c1ea18c
Iscle <albertiscle9@gmail.com>
2020-11-01 02:17:31 +0100

    BaseActivity.java: Create constructors
    Signed-off-by: Iscle <albertiscle9@gmail.com>

--- mcinabox/src/main/java/com/aof/mcinabox/BaseActivity.java
+++ mcinabox/src/main/java/com/aof/mcinabox/BaseActivity.java
@@ -3,6 +3,14 @@ package com.aof.mcinabox;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+    public BaseActivity() {
+        super();
+    }
+
+    public BaseActivity(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
     public MCinaBox getMCinaBox() {
         return (MCinaBox) getApplication();
     }
