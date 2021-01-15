afaf82efd9fec14e1931dc8c249fec2178542d59
longjunyu2 <877730493@qq.com>
2021-01-15 19:46:46 +0800

    [app] 添加屏幕尺寸计算方法

--- app/src/main/java/com/aof/mcinabox/utils/DisplayUtils.java
+++ app/src/main/java/com/aof/mcinabox/utils/DisplayUtils.java
@@ -1,22 +1,60 @@
 package com.aof.mcinabox.utils;
 
 import android.content.Context;
+import android.content.res.Resources;
+
+import java.lang.reflect.Method;
 
 public class DisplayUtils {
 
     public static int getPxFromDp(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
-        return (int)(dpValue * scale);
+        return (int) (dpValue * scale);
     }
 
-    public static float getDpFromPx(Context context, float pxValue){
+    public static float getDpFromPx(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (pxValue / scale);
     }
 
-    public static int getPxFromSp(Context context, float spValue){
+    public static int getPxFromSp(Context context, float spValue) {
         final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int)(spValue * fontScale + 0.5f);
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+
+    public static boolean checkDeviceHasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return hasNavigationBar;
+    }
+
+    public static int getNavigationBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height","dimen", "android");
+        return resources.getDimensionPixelSize(resourceId);
+    }
+
+    public static int[] getApplicationWindowSize(Context context){
+        int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+        int screenHeight = context.getResources().getDisplayMetrics().heightPixels;
+        return new int[]{screenWidth, screenHeight};
     }
 
 }
